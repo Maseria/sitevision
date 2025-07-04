eb7e154f33c0631fc893510ab884f275cf5a93b8
unknown <maseriaeugene@gmail.com>
2025-07-04 15:46:39 +0300

    debug get

--- js/index.js
+++ js/index.js
@@ -3,8 +3,10 @@ var API_ENDPOINT = "https://5uqk927m6e.execute-api.us-east-1.amazonaws.com/prod"
 
 // AJAX POST request to save student data
 document.getElementById("saveIncident").onclick = function(){
+    const incidentid = String(Math.floor(Math.random() * 1000)); // Random
+    
     var inputData = {
-        "incidentid": $('#incidentid').val(),
+        "incidentid": incidentid,
         "title": $('#title').val(),
         "date": $('#date').val(),
         "description": $('#description').val()
